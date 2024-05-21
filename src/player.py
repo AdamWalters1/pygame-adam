1b6a1338d8f6f4addfc60b2faf5cc3b913936cc8
Adam Watlers <adamwalters2006@gmail.com>
2024-05-20 23:48:57 -0400

    Updated movement, broke graphics in process

--- src/player.py
+++ src/player.py
@@ -10,22 +10,37 @@ class Player:
         self.speed = speed
         self.car_images = car_images
         self.direction = 1
+        self.current_speed = 0
+        self.angle = 0  # Angle in degrees (0, 90, 180, 270)
+        
     def move(self, keys):
+        
         if keys[pygame.K_UP]:
-            self.y -= self.speed
-            self.direction = 11
-        if keys[pygame.K_DOWN]:
-            self.y += self.speed
-            self.direction = 12
+            self.current_speed = self.speed
+        elif keys[pygame.K_DOWN]:
+            self.current_speed = -self.speed
+        else:
+            self.current_speed = 0
         if keys[pygame.K_LEFT]:
-            self.x -= self.speed
-            self.direction = 9
-        if keys[pygame.K_RIGHT]:
-            self.x += self.speed
-            self.direction = 10
+            self.angle = (self.angle - 90) % 360
+        elif keys[pygame.K_RIGHT]:
+            self.angle = (self.angle + 90) % 360
+
+        if self.angle == 0:
+            self.y -= self.current_speed
+            direction = 9
+        elif self.angle == 90:
+            self.x += self.current_speed
+            direction = 11
+        elif self.angle == 180:
+            self.y += self.current_speed
+            direction = 12
+        elif self.angle == 270:
+            self.x -= self.current_speed
+            direction = 10
+
 
     def draw(self, surface):
         car_image = self.car_images[self.direction-1]
         surface.blit(car_image, (self.x, self.y))
-    #def draw(self, surface):
-        #pygame.draw.rect(surface, (255, 0, 0), (self.x, self.y, self.size, self.size))
+    
